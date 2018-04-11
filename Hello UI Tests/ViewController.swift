7e974b1df3315ac5a78373f3d34573851e1479d3
Tomasz Wójcik <wojcik.tomek@gmail.com>
2018-04-11 16:03:31 +0200

    Some tests

--- Hello UI Tests/ViewController.swift
+++ Hello UI Tests/ViewController.swift
@@ -9,6 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet private weak var emailTextField: UITextField!
+    @IBOutlet private weak var emailHintLabel: UILabel!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var passwordHintLabel: UILabel!
+
+    @IBAction func performLogin(_ sender: UIButton) {
+        sender.isEnabled = false
+        defer {
+            print("Enabling button")
+            sender.isEnabled = true
+        }
+        resetHints()
+        guard validateFields() else { return }
+        if validateCredentials() {
+            performSegue(withIdentifier: "showLoggedInScreen", sender: nil)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +37,49 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    private func resetHints() {
+        UIView.animate(withDuration: 0.25) {
+            self.emailHintLabel.isHidden = true
+            self.passwordHintLabel.isHidden = true
+        }
+    }
 
+    private func validateFields() -> Bool {
+        var result = true
+        if emailTextField.text!.isEmpty {
+            showEmailHint(message: "Cannot be blank")
+            result = false
+        }
+
+        if passwordTextField.text!.isEmpty {
+            showPasswordHint(message: "Cannot be blank")
+            result = false
+        }
+        return result
+    }
+
+    private func validateCredentials() -> Bool {
+        if emailTextField.text == "user" && passwordTextField.text == "secret" {
+            return true
+
+        } else {
+            showPasswordHint(message: "Invalid credentials")
+            return false
+        }
+    }
+
+    private func showEmailHint(message: String) {
+        UIView.animate(withDuration: 0.25) {
+            self.emailHintLabel.text = message
+            self.emailHintLabel.isHidden = false
+        }
+    }
+
+    private func showPasswordHint(message: String) {
+        UIView.animate(withDuration: 0.25) {
+            self.passwordHintLabel.text = message
+            self.passwordHintLabel.isHidden = false
+
+        }
+    }
 }
-
--- Hello UI TestsUITests/Hello_UI_TestsUITests.swift
+++ Hello UI TestsUITests/Hello_UI_TestsUITests.swift
@@ -9,28 +9,53 @@
 import XCTest
 
 class Hello_UI_TestsUITests: XCTestCase {
+    var app: XCUIApplication!
         
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = XCUIApplication()
+        app.launch()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testLoginWithEmptyCredentials() {
+        let loginButton = app.buttons["Log in"]
+        loginButton.tap()
+
+        let error = app.staticTexts["Cannot be blank"]
+        XCTAssertTrue(error.exists)
     }
-    
+
+    func testLoginWithInvalidCredentials() {
+        let emailTextField = app.textFields["E-mail"]
+        emailTextField.tap()
+        emailTextField.typeText("invalid_user")
+
+        let passwordSecureTextField = app.secureTextFields["Password"]
+        passwordSecureTextField.tap()
+        passwordSecureTextField.typeText("secret")
+
+        app.buttons["Log in"].tap()
+        let error = app.staticTexts["Invalid credentials"]
+        XCTAssertTrue(error.exists)
+    }
+
+    func testSuccessfulLogin() {
+        let emailTextField = app.textFields["E-mail"]
+        emailTextField.tap()
+        emailTextField.typeText("user")
+
+        let passwordSecureTextField = app.secureTextFields["Password"]
+        passwordSecureTextField.tap()
+        passwordSecureTextField.typeText("secret")
+
+        app.buttons["Log in"].tap()
+
+        let loggedInMessage = app.staticTexts["Logged in!"]
+        XCTAssertTrue(loggedInMessage.exists)
+    }
 }
